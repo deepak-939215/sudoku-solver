2da8fb6f2f9799df09767fb879634fe09aee6923
MBSDeepak1212 <mbsdeepak1@gmail.com>
2022-07-06 15:35:37 +0530

    Did it

--- dancingLinks.h
+++ dancingLinks.h
@@ -2,13 +2,73 @@
 
 class DancingLinks{
 public:
-	DancingLinks();
+
 	Node head;
 	Node* headNode;
 	bool isSolved;
 	bool **cover;
+
+
+	DancingLinks(){
+		isSolved=false;
+		headNode=&head;
+	};
 	
-	void coverCol(Node*);
-	void uncoverCol(Node *);
-	bool search(int,int**);
-}
+	
+	void coverCol(Node* col){
+		col->l->r=col->r;
+		col->r->l=col->l;   // hide the column
+
+		for(Node* nv=col->d;nv!=col;nv=nv->d){		//Vertical Traversal
+			for(Node* nh=nv->r;nh!=nv;nh=nh->r){
+				nh->d->u=nh->u;
+				nh->u->d=nh->d;
+
+				nh->head->size--;
+			}
+		}
+
+	}
+	void uncoverCol(Node* col){
+		col->l->r=col;
+		col->r->l=col;
+
+		for(Node *nv=col->u;nv!=col;nv=nv->u){
+			for(Node *nh=nv->l;nh!=nv;nh=nh->l){
+				nh->d->u=nh;
+				nh->u->d=nh;
+
+				nh->head->size++;
+			}
+		}
+	}
+	bool search(int sc,int** sudoku){
+		if(headNode->r==headNode){
+			return isSolved=true;
+		}
+
+		Node* col=headNode->r;
+		for(Node* nh=col->r;nh!=headNode;nh=nh->r)
+			if(nh->size<col->size)
+				col=nh;
+
+		coverCol(col);
+
+		for(Node* nv=col->d;nv!=col;nv=nv->d){
+			for(Node* nh=nv->r;nh!=nv;nh->r){
+				coverCol(nh->head);
+			}
+			if(search(sc+1,sudoku)){
+				sudoku[nv->rowEl[1]-1][nv->rowEl[2]-1]=nv->rowEl[0];
+				return true;
+			}
+			for(Node* nh=nv->l;nh!=nv;nh->l){
+				uncoverCol(nh->head);
+			}
+		}
+
+		uncoverCol(col);
+
+		return false;
+	}
+};
--- node.h
+++ node.h
@@ -1,7 +1,9 @@
 class Node{
 public:
-	Node();
+	Node(){
+		
+	};
 	Node *l,*r,*u,*d,*head;
 	int size;	 		// for size of column
 	int rowEl[3];		//row elements (1 to size, R, C)	
-}
+};
